85aa269770d1c5211ffe53707b11b5d9c8ad829c
hiroki022 <s1701078@sendai-nct.jp>
2020-11-19 23:27:03 +0900

    new

--- urls.py
+++ urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-#import hello.views as hello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
+    path('yoyaku_system/', include('yoyaku_system.urls')),
 ]
